fcea1019b1f68f1e0b6f4d59c94fedda936446c9
DESKTOP-KKQKAE4\luyao <luyao@live.ca>
2021-06-18 00:51:03 -0400

    OCP demo1

---
+++ src/main/java/com/example/ocp_s1/OcpS1Application.java
@@ -0,0 +1,13 @@
+package com.example.ocp_s1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcpS1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OcpS1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ocp_s1/OcpS1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ocp_s1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcpS1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
